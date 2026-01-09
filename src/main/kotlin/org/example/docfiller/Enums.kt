04d44c000486a20483cbacb7455b48b75d0a70ff
HP <saidovdiyer777@gmail.com>
2026-01-09 17:14:37 +0500

    add: error code

--- src/main/kotlin/org/example/docfiller/Enums.kt
+++ src/main/kotlin/org/example/docfiller/Enums.kt
@@ -3,3 +3,10 @@ package org.example.docfiller
 enum class UserRole{
     ROLE_USER, ROLE_ADMIN
 }
+
+enum class ErrorCode(val code: Int, val message: String) {
+    //1
+    EMPLOYEE_NOT_FOUND(100, "EMPLOYEE_NOT_FOUND"),
+    EMPLOYEE_ALREADY_EXISTS(101, "EMPLOYEE_ALREADY_EXISTS"),
+    EMPLOYEE_NOT_AUTHENTICATED(102, "EMPLOYEE_NOT_AUTHENTICATED"),
+}
