1a97a54a71ea483423bcc079462477b1ab16751b
Javokhirkh <javohir23.09.2003@gmail.com>
2026-01-09 16:18:00 +0500

    intial changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    kotlin("jvm") version "2.2.21"
-    kotlin("plugin.spring") version "2.2.21"
-    id("org.springframework.boot") version "4.0.1"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.4.12"
     id("io.spring.dependency-management") version "1.1.7"
-    kotlin("plugin.jpa") version "2.2.21"
+    kotlin("plugin.jpa") version "1.9.25"
 }
 
 group = "org.example"
--- src/main/kotlin/org/example/docfiller/DocFillerApplication.kt
+++ src/main/kotlin/org/example/docfiller/DocFillerApplication.kt
@@ -2,8 +2,12 @@ package org.example.docfiller
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableJpaRepositories(repositoryBaseClass = BaseRepositoryImpl::class)
 class DocFillerApplication
 
 fun main(args: Array<String>) {
