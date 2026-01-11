074e976a24ff532dbe15264f86520c821cfd4c5b
HP <saidovdiyer777@gmail.com>
2026-01-12 00:32:26 +0500

    feat(Employee): CRUD

--- src/main/kotlin/org/example/docfiller/dtos/EmployeeDtos.kt
+++ src/main/kotlin/org/example/docfiller/dtos/EmployeeDtos.kt
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Pattern
 import jakarta.validation.constraints.Size
+import org.example.docfiller.UserRole
 
 data class LoginResponse(
     val accessToken: String,
@@ -42,4 +43,40 @@ data class RegisterRequest(
     @field:NotNull
     @field:Min(value = 1)
     var organizationId: Long
+)
+
+data class EmployeeCreate(
+    @field:NotBlank
+    @field:Size(min = 4, max = 50)
+    val firstName: String,
+    @field:Size(min = 1, max = 50)
+    val lastName: String? = null,
+    @field:NotBlank
+    @field:Pattern(
+        regexp = "^\\+998(90|91|93|94|95|97|98|99|33|50|88)\\d{7}$",
+        message = "phone.number.regex"
+    )
+    val phoneNumber: String,
+    @field:NotBlank
+    @field:Size(min = 6, max = 50)
+    val password: String,
+    @field:NotBlank
+    val role: UserRole,
+    @field:Min(value = 1)
+    var organizationId: Long? = null,
+)
+
+data class EmployeeResponse(
+    val id: Long,
+    val firstName: String,
+    val lastName: String,
+    val phoneNumber: String,
+    val organizationId: Long,
+)
+
+data class EmployeeUpdate(
+    val firstName: String? = null,
+    val lastName: String? = null,
+    val phoneNumber: String? = null,
+    val organizationId: Long? = null,
 )
--- src/main/kotlin/org/example/docfiller/services/EmployeeService.kt
+++ src/main/kotlin/org/example/docfiller/services/EmployeeService.kt
@@ -8,6 +8,9 @@ import org.example.docfiller.OrganizationNotFoundException
 import org.example.docfiller.OrganizationRepository
 import org.example.docfiller.PhoneNumberAlreadyExists
 import org.example.docfiller.UserRole
+import org.example.docfiller.dtos.EmployeeCreate
+import org.example.docfiller.dtos.EmployeeResponse
+import org.example.docfiller.dtos.EmployeeUpdate
 import org.example.docfiller.dtos.LoginRequest
 import org.example.docfiller.dtos.LoginResponse
 import org.example.docfiller.dtos.RegisterRequest
@@ -18,6 +21,10 @@ import org.springframework.stereotype.Service
 interface EmployeeService {
     fun login(request: LoginRequest): LoginResponse
     fun register(request: RegisterRequest)
+    fun create(request: EmployeeCreate)
+    fun getOne(id: Long): EmployeeResponse
+    fun update(id: Long, request: EmployeeUpdate)
+    fun delete(id: Long)
 }
 
 @Service
@@ -59,5 +66,87 @@ class EmployeeServiceImpl(
             }
         }
         throw OrganizationNotFoundException()
+    }
+
+    override fun create(request: EmployeeCreate) {
+        repository.findByPhoneNumberAndDeletedFalse(request.phoneNumber)?.let {
+            throw PhoneNumberAlreadyExists()
+        }
+        request.organizationId?.let { organizationId ->
+            organizationRepo.findByIdAndDeletedFalse(organizationId)?.let { organization ->
+                repository.save(Employee(
+                    firstName = request.firstName,
+                    lastName = request.lastName?.let { it } as String,
+                    phoneNumber = request.phoneNumber,
+                    password = passwordEncoder.encode(request.password),
+                    role = request.role,
+                    organization = organization,
+                ))
+                return
+            }
+            throw OrganizationNotFoundException()
+        }
+        repository.save(Employee(
+            firstName = request.firstName,
+            lastName = request.lastName?.let { it } as String,
+            phoneNumber = request.phoneNumber,
+            password = passwordEncoder.encode(request.password),
+            role = request.role,
+        ))
+        return
+    }
+
+    override fun getOne(id: Long): EmployeeResponse {
+        repository.findByIdAndDeletedFalse(id)?.let { employee ->
+            return EmployeeResponse(
+                employee.id!!,
+                employee.firstName,
+                employee.lastName,
+                employee.phoneNumber,
+                employee.organization?.let { it.id } as Long
+            )
+        }
+        throw EmployeeNotFoundException()
+    }
+
+    override fun update(id: Long, request: EmployeeUpdate) {
+        repository.findByIdAndDeletedFalse(id)?.let { employee ->
+            request.run {
+                //set phone number
+                phoneNumber?.let { requestPhone ->
+                    repository.findByPhoneNumberAndDeletedFalse(phoneNumber)?.let { phone ->
+                        throw PhoneNumberAlreadyExists()
+                    }
+                    employee.phoneNumber = requestPhone
+                }
+                //set organization
+                organizationId?.let { organizationId ->
+                    organizationRepo.findByIdAndDeletedFalse(organizationId)?.let { organization ->
+                        employee.organization = organization
+                    }
+                    throw OrganizationNotFoundException()
+                }
+
+                firstName?.let { requestFirstName ->
+                    employee.firstName = requestFirstName
+                }
+
+                lastName?.let { requestLastName ->
+                    employee.lastName = requestLastName
+                }
+
+                repository.save(employee)
+                return
+            }
+        }
+        throw EmployeeNotFoundException()
+    }
+
+    override fun delete(id: Long) {
+        repository.findByIdAndDeletedFalse(id)?.let { employee ->
+            repository.trash(id)
+            return
+        }
+        throw EmployeeNotFoundException()
     }
 }
