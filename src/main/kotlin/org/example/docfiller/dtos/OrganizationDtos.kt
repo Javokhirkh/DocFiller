c4935250c8e1f28bc9ef57a41942f514f81a0ace
Javokhirkh <javohir23.09.2003@gmail.com>
2026-01-12 17:20:53 +0500

    add : Config to error messages , add: validation.properties

--- src/main/kotlin/org/example/docfiller/dtos/OrganizationDtos.kt
+++ src/main/kotlin/org/example/docfiller/dtos/OrganizationDtos.kt
@@ -5,15 +5,19 @@ import jakarta.validation.constraints.Pattern
 import jakarta.validation.constraints.Size
 import java.time.LocalDateTime
 
+
 data class OrganizationCreate(
-    @field:NotBlank
-    @field:Size(min = 3, max = 100)
+
+    @field:NotBlank(message = "{organization.name.notBlank}")
+    @field:Size(min = 3, max = 100, message = "{organization.name.size}")
     val name: String,
+
+    @field:NotBlank(message = "{organization.phoneNumber.notBlank}")
     @field:Pattern(
         regexp = "^\\+998(90|91|93|94|95|97|98|99|33|50|88)\\d{7}$",
-        message = "phone.number.regex"
+        message = "{phone.number.regex}"
     )
-    val phoneNumber: String,
+    val phoneNumber: String
 )
 
 data class OrganizationResponse(
